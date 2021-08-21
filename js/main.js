cfad0c668d5cafe5da4293425b0d1e1549701d6e
mdhainf20 <mdhanif115825@gmail.com>
2021-08-21 18:13:44 +0600

    added comment

--- js/main.js
+++ js/main.js
@@ -1,79 +1,143 @@
-// memory cost function start here 
+ /*------------------
+  memory price function start here 
+  ------------------- */
 function memoryCost(memoryType){
-    const memoryCost = document.getElementById(memoryType);
-    const highMemorycost = document.getElementById("high-memory");
-    const previousMemoryExpense = document.getElementById("previous-memory-cost");
-    if(memoryCost==highMemorycost){
-        previousMemoryExpense.innerText = 180;
+    // memory price button call here 
+    const memoryInputPrice = document.getElementById(memoryType);
+    // Memory 16GB button call here 
+    const memoryPrice16gb = document.getElementById("memory-16gb");
+    // Memory price showing place call here 
+    const memoryPrice = document.getElementById("previous-memory-cost");
+    // for 16GB memory price is change here
+    if(memoryInputPrice==memoryPrice16gb){
+        memoryPrice.innerText = 180;
     }
+    // for 8GB Memory price is change here
     else{
-        previousMemoryExpense.innerText = 0;
+        memoryPrice.innerText = 0;
     }
-    return total();
+    // calculation total price function call here 
+    total();
 }
-// memory cost function end here 
+ /*----------------------
+ memory price function end here 
+ ------------------------- */
 
-// storage cost function start here  
+
+
+ 
+/*---------------------
+ storage price function start here 
+ --------------------- */ 
 function storageCost(stroageType){
-    const storageCost = document.getElementById(stroageType);
-    const lowStorageCost = document.getElementById("low-storage");
-    const midStorageCost = document.getElementById("mid-storage");
-    const heighStorageCost = document.getElementById("high-storage");
-    const previousStorageCost = document.getElementById("previous-storage-cost");
-    if(storageCost==lowStorageCost){
-        previousStorageCost.innerText = 0;
+    // storage button call here 
+    const storagePrice = document.getElementById(stroageType);
+    // Storage Button 256GB call here 
+    const storagePrice256gb = document.getElementById("storage-256gb");
+     // Storage Button 512GB call here 
+    const storagePrice512gb = document.getElementById("storage-512gb");
+     // Storage Button 1TB call here 
+    const storagePrice1tb = document.getElementById("storage-1tb");
+    // storage price showing place call here 
+    const storagePriceReset = document.getElementById("previous-storage-cost");
+     // for 256GB storage price is change here
+    if(storagePrice==storagePrice256gb){
+        storagePriceReset.innerText = 0;
     }
-    else if(storageCost==midStorageCost){
-        previousStorageCost.innerText = 100;
+    // for 512GB storage price is change here
+    else if(storagePrice==storagePrice512gb){
+        storagePriceReset.innerText = 100;
     }
-    else if(storageCost==heighStorageCost){
-        previousStorageCost.innerText = 180;
+    // for 1TB storage price is change here
+    else if(storagePrice==storagePrice1tb){
+        storagePriceReset.innerText = 180;
     }
-    return total();
+     // calculation total price function call here 
+    total();
 }
-// storage cost function end here  
+/*-------------------------
+  storage price function end here   
+  ------------------------*/
 
-// delivery cost function start here 
+
+
+/* -------------------
+delivery cost function start here  
+-------------------------*/
 function deliveyCost(delivery){
+    // delivery button call here 
     const deliveryCost = document.getElementById(delivery);
+    // free delivery button call here 
     const freeDelivery = document.getElementById("free-delivery");
-    const previousDelivery = document.getElementById('previous-delivery');
+    // delivery cost showing place call here 
+    const previousFee = document.getElementById('delivery-fee');
+    // for free delivery 
     if(deliveryCost==freeDelivery){
-        previousDelivery.innerText = 0;
+        previousFee.innerText = 0;
     }
+    // for argent delivery 
     else{
-        previousDelivery.innerText = 20;
+        previousFee.innerText = 20;
     }
-    return total();
+    // calculation total price function call here 
+    total();
 }
-// delivery cost function end here 
+/*--------------------
+ delivery cost function end here 
+ --------------------*/ 
 
-// calculation the product price start here 
+
+
+ /*--------------------
+  calculation the product price start here
+  ---------------------  */
 function total(){
+    // regular price area call here 
     const regularPrice = document.getElementById("regular-price").innerText;
-    const changedMemoryCost =document.getElementById('previous-memory-cost').innerText;
-    const changedStorageCost = document.getElementById("previous-storage-cost").innerText;
-    const changedDeliveryFee = document.getElementById("previous-delivery").innerText;
-    const previousTotalPrice = document.getElementById('previous-total');
-    const beforePomoFinalTotal = document.getElementById("before-pomo");
-    const totalPrice = parseFloat(regularPrice) + parseFloat(changedMemoryCost) + parseFloat(changedStorageCost) + parseFloat(changedDeliveryFee );
-    previousTotalPrice.innerText = totalPrice;
-    beforePomoFinalTotal.innerText = totalPrice;
+    // final memory price area call here 
+    const finalMemoryPrice =document.getElementById('previous-memory-cost').innerText;
+    // final storage price area call here 
+    const finalStoragePrice = document.getElementById("previous-storage-cost").innerText;
+    // final delivery charge area call here 
+    const finalDeliveryFee = document.getElementById("delivery-fee").innerText;
+    // total price showing area call here 
+    const totalPrice = document.getElementById('previous-total');
+    // pomo total price showing here 
+    const pomoFinalTotal = document.getElementById("before-pomo");
+    // total price calculation 
+    const sumTotalPrice = parseFloat(regularPrice) + parseFloat(finalMemoryPrice) + parseFloat(finalStoragePrice) + parseFloat(finalDeliveryFee );
+    totalPrice.innerText = sumTotalPrice;
+    pomoFinalTotal.innerText = sumTotalPrice;
 }
-// calculation the product price end here 
+/*----------------
+ calculation the product price end here  
+ -----------------*/
 
-// pomo code function start here
+
+
+/*-------------------
+ pomo code function start here
+ ------------------- */
 function pomoCode(){
+    // pomo input call here 
     const pomoInput = document.getElementById("pomo-input");
     const pomoInputValue = pomoInput.value;
+    // sellor given pomo code here 
     const sellorPomo = 'stevekaku';
+    // compare with sellor pomo code and customer pomo code 
     if(pomoInputValue == sellorPomo){
-       const totalEditPrice = document.getElementById('previous-total').innerText;
-       const beforePomoTotalPrice = document.getElementById('before-pomo');
-       const afterPomoTotalDescount = parseFloat(totalEditPrice)*20/100;
-       const afterPomoTotalPrice = parseFloat(totalEditPrice) - afterPomoTotalDescount;
-       beforePomoTotalPrice.innerText = afterPomoTotalPrice;
+        // total amount call here 
+       const totalSumPrice = document.getElementById('previous-total').innerText;
+        // after using pomo code price showing place call here 
+       const pomoDiscountPrice = document.getElementById('before-pomo');
+        // 20% discount by using pomo code 
+       const afterPomoTotalDescount = parseFloat(totalSumPrice)*20/100;
+        // pomo discount amount subtruncate and get update price
+       const afterPomoTotalPrice = parseFloat(totalSumPrice) - afterPomoTotalDescount;
+       pomoDiscountPrice.innerText = afterPomoTotalPrice;
        pomoInput.value='';
     }
 } 
-// pomo code function end here 
+/* ------------------
+pomo code function end here 
+-------------------- */
